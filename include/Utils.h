b0fa05a0278a24c22745ff5306f39739b5f577a3
Daniel <daniel.gonzalez@desy.de>
2015-07-29 17:11:40 +0200

    add utils

--- include/Utils.h
+++ include/Utils.h
@@ -7,12 +7,12 @@
 
 template<typename P>
 inline void sort_by_eta(std::vector<P> & particles){
-    std::sort(particles.begin(), particles.end(), [](const P & p1, const P & p2){return p1.eta() > p2.eta();});
+  std::sort(particles.begin(), particles.end(), [](const P & p1, const P & p2){return fabs(p1.eta()) > fabs(p2.eta());});
 }
 
 template<typename P>
 inline void sort_by_eta(std::vector<P*> & particles){
-    std::sort(particles.begin(), particles.end(), [](const P* p1, const P* p2){return p1->eta() > p2->eta();});
+  std::sort(particles.begin(), particles.end(), [](const P* p1, const P* p2){return fabs(p1->eta()) > fabs(p2->eta());});
 }
 
 
