8f337786efb596782a0831d8f66bed80efae7c6c
Daniel <daniel.gonzalez@desy.de>
2015-07-29 17:11:08 +0200

    add test hists

--- include/VLQToTopAndLeptonHists.h
+++ include/VLQToTopAndLeptonHists.h
@@ -11,12 +11,15 @@
  */
 
 class VLQToTopAndLeptonHists: public uhh2::Hists {
-public:
+ public:
     // use the same constructor arguments as Hists for forwarding:
     VLQToTopAndLeptonHists(uhh2::Context & ctx, const std::string & dirname);
 
     virtual void fill(const uhh2::Event & ev) override;
     virtual ~VLQToTopAndLeptonHists();
-
-
+ private:
+    TH1F* N_jets, *eta_jet1, *eta_jet2, *eta_jet3, *eta_jet4;
+    TH2F* pt_eta_jet1, *pt_eta_jet2, *pt_eta_jet3, *pt_eta_jet4;
+    TH1F* N_mu, *pt_mu, *eta_mu, *reliso_mu;
+    TH1F* N_pv;
 };
--- src/VLQToTopAndLeptonHists.cxx
+++ src/VLQToTopAndLeptonHists.cxx
@@ -2,6 +2,7 @@
 #include "UHH2/core/include/Event.h"
 
 #include "TH1F.h"
+#include "TH2F.h"
 #include <iostream>
 
 using namespace std;
@@ -10,20 +11,25 @@ using namespace uhh2;
 VLQToTopAndLeptonHists::VLQToTopAndLeptonHists(Context & ctx, const string & dirname): Hists(ctx, dirname){
   // book all histograms here
   // jets
-  book<TH1F>("N_jets", "N_{jets}", 20, 0, 20);  
-  book<TH1F>("eta_jet1", "#eta^{jet 1}", 40, -2.5, 2.5);
-  book<TH1F>("eta_jet2", "#eta^{jet 2}", 40, -2.5, 2.5);
-  book<TH1F>("eta_jet3", "#eta^{jet 3}", 40, -2.5, 2.5);
-  book<TH1F>("eta_jet4", "#eta^{jet 4}", 40, -2.5, 2.5);
+  N_jets = book<TH1F>("N_jets", "N_{jets}", 20, 0, 20); 
+  eta_jet1 = book<TH1F>("eta_jet1", "#eta^{jet 1}", 40, -5, 5);
+  eta_jet2 = book<TH1F>("eta_jet2", "#eta^{jet 2}", 40, -5, 5);
+  eta_jet3 = book<TH1F>("eta_jet3", "#eta^{jet 3}", 40, -5, 5);
+  eta_jet4 = book<TH1F>("eta_jet4", "#eta^{jet 4}", 40, -5, 5);
 
+  pt_eta_jet1 = book<TH2F>("pt_eta_jet1", "x: p_{T}, y: #eta jet 1", 100, 0, 150, 100, 0, 5);
+  pt_eta_jet2 = book<TH2F>("pt_eta_jet2", "x: p_{T}, y: #eta jet 2", 100, 0, 150, 100, 0, 5);
+  pt_eta_jet3 = book<TH2F>("pt_eta_jet3", "x: p_{T}, y: #eta jet 3", 100, 0, 150, 100, 0, 5);
+  pt_eta_jet4 = book<TH2F>("pt_eta_jet4", "x: p_{T}, y: #eta jet 4", 100, 0, 150, 100, 0, 5);
+  
   // leptons
-  book<TH1F>("N_mu", "N^{#mu}", 10, 0, 10);
-  book<TH1F>("pt_mu", "p_{T}^{#mu} [GeV/c]", 40, 0, 200);
-  book<TH1F>("eta_mu", "#eta^{#mu}", 40, -2.1, 2.1);
-  book<TH1F>("reliso_mu", "#mu rel. Iso", 40, 0, 0.5);
+  N_mu = book<TH1F>("N_mu", "N^{#mu}", 10, 0, 10);
+  pt_mu = book<TH1F>("pt_mu", "p_{T}^{#mu} [GeV/c]", 40, 0, 200);
+  eta_mu = book<TH1F>("eta_mu", "#eta^{#mu}", 40, -2.4, 2.4);
+  reliso_mu = book<TH1F>("reliso_mu", "#mu rel. Iso", 40, 0, 0.5);
 
   // primary vertices
-  book<TH1F>("N_pv", "N^{PV}", 50, 0, 50);
+  N_pv = book<TH1F>("N_pv", "N^{PV}", 50, 0, 50);
 }
 
 
@@ -38,31 +44,37 @@ void VLQToTopAndLeptonHists::fill(const Event & event){
   
   std::vector<Jet>* jets = event.jets;
   int Njets = jets->size();
-  hist("N_jets")->Fill(Njets, weight);
+  N_jets->Fill(Njets, weight);
   
   if(Njets>=1){
-    hist("eta_jet1")->Fill(jets->at(0).eta(), weight);
+    eta_jet1->Fill(jets->at(0).eta(), weight);
+    pt_eta_jet1->Fill(jets->at(0).pt(),fabs(jets->at(0).eta()),weight);
   }
   if(Njets>=2){
-    hist("eta_jet2")->Fill(jets->at(1).eta(), weight);
+    eta_jet2->Fill(jets->at(1).eta(), weight);
+    pt_eta_jet2->Fill(jets->at(1).pt(),fabs(jets->at(1).eta()),weight);
   }
   if(Njets>=3){
-    hist("eta_jet3")->Fill(jets->at(2).eta(), weight);
+    eta_jet3->Fill(jets->at(2).eta(), weight);
+    pt_eta_jet3->Fill(jets->at(2).pt(),fabs(jets->at(2).eta()),weight);
   }
   if(Njets>=4){
-    hist("eta_jet4")->Fill(jets->at(3).eta(), weight);
+    eta_jet4->Fill(jets->at(3).eta(), weight);
+    pt_eta_jet4->Fill(jets->at(3).pt(),fabs(jets->at(3).eta()),weight);
   }
 
+  
+
   int Nmuons = event.muons->size();
-  hist("N_mu")->Fill(Nmuons, weight);
+  N_mu->Fill(Nmuons, weight);
   for (const Muon & thismu : *event.muons){
-      hist("pt_mu")->Fill(thismu.pt(), weight);
-      hist("eta_mu")->Fill(thismu.eta(), weight);
-      hist("reliso_mu")->Fill(thismu.relIso(), weight);
+      pt_mu->Fill(thismu.pt(), weight);
+      eta_mu->Fill(thismu.eta(), weight);
+      reliso_mu->Fill(thismu.relIso(), weight);
   }
   
   int Npvs = event.pvs->size();
-  hist("N_pv")->Fill(Npvs, weight);
+  N_pv->Fill(Npvs, weight);
 }
 
 VLQToTopAndLeptonHists::~VLQToTopAndLeptonHists(){}
