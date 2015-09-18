e18a87297a26e5d6027abf55764804985faa78ab
Daniel <daniel.gonzalez@desy.de>
2015-09-18 19:14:45 +0200

    build dicts for root

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 LIBRARY := SUHH2VLQToTopAndLepton
+DICT := include/BprimeContainer.h include/BprimeGenContainer.h include/SUHH2VLQToTopAndLepton_LinkDef.h
 USERLDFLAGS := -lSUHH2core -lSUHH2common -lGenVector
 # enable par creation; this is necessary for all packages containing AnalysisModules
 # to be loaded from by AnalysisModuleRunner.
---
+++ include/SUHH2VLQToTopAndLepton_LinkDef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+
+#pragma link C++ class BprimeContainer+;
+#pragma link C++ class std::vector<BprimeContainer>+;
+#pragma link C++ class BprimeGenContainer+;
+#pragma link C++ class std::vector<BprimeGenContainer>+;
+
+
+#endif // __CINT__
