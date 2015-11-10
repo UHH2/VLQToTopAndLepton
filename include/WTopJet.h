5eade832e08c21b63ea9709c0f688abb47f7a62b
Daniel <daniel.gonzalez@desy.de>
2015-11-10 15:45:27 +0100

    w tagger

---
+++ include/WTopJet.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+#include "UHH2/core/include/Utils.h" 
+#include "UHH2/core/include/GenParticle.h"
+#include "UHH2/core/include/Selection.h"
+
+
+class WMass {
+public:
+  explicit WMass(double min_=50,double max_=120): min(min_),max(max_){}
+    
+    bool operator()(const TopJet & topjet, const uhh2::Event & event) const;
+    
+private:
+  double min, max;
+};
---
+++ src/WTopJet.cxx
@@ -0,0 +1,12 @@
+#include "UHH2/VLQToTopAndLepton/include/WTopJet.h"
+
+
+bool WMass::operator()(TopJet const & topjet, uhh2::Event const & event) const {
+  LorentzVector Candidate (0,0,0,0);
+  for(auto & subjet : topjet.subjets()){
+    Candidate += subjet.v4();
+  }
+  if(Candidate.M2()> min*min && Candidate.M2()<max*max)
+    return true;
+  return false;
+}
